f4006dc8b351802872a8841572d6cc2ce02d7c8e
salihov <salihoff.ildar2015@ya.ru>
2024-01-24 11:36:14 +0500

    KS-1-2 do smth

---
+++ src/main/kotlin/lesson_1/lesson_2Task.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_1
+
+fun main() {
+    val shopOfOrder1 = 75
+    val congratulationsText = "Спасибо за покупку!"
+
+    println("Количество заказов: $shopOfOrder1")
+    println("Текст с благодарностью:$congratulationsText")
+
+    //val person = 2000
+    //println("Количество работников:$person")
+
+    val person = 2000-1
+
+
+    //один работник уволился
+    println("Количество работников: $person")
+
+}
